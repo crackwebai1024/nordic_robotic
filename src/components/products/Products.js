39da13e3cce8f1fac3258d9c61be7be68a327588
crackwebai1024 <webtowardapex@outlook.com>
2020-08-24 04:18:08 +0800

    delete yarn

--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -2,12 +2,14 @@ import React from "react";
 import styles from "./products.module.css";
 import SideBar from "./components/SideBar";
 import { MDBBox, MDBRow } from "mdbreact";
+import Content from "./components/Content";
 
 const Products = (props) => {
   return (
     <MDBBox className="cont_container">
       <MDBRow>
         <SideBar />
+        <Content />
       </MDBRow>
     </MDBBox>
   );
